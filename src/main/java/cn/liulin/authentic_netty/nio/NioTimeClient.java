b9b0da347c939d9ea80838596d9c1dbaa0b16a4e
liulin <liulin@gmail.com>
2021-03-26 17:29:09 +0800

    story# init NIO client

---
+++ src/main/java/cn/liulin/authentic_netty/nio/NioTimeClient.java
@@ -0,0 +1,14 @@
+package cn.liulin.authentic_netty.nio;
+
+/**
+ * cn.liulin.authentic_netty.nio$
+ * nio客户端
+ * @author ll
+ * @date 2021-03-26 16:28:18
+ **/
+public class NioTimeClient {
+    public static void main(String[] args) {
+        int port = 8080;
+        new Thread().start();
+    }
+}
---
+++ src/main/java/cn/liulin/authentic_netty/nio/TimeClientHandler.java
@@ -0,0 +1,118 @@
+package cn.liulin.authentic_netty.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * cn.liulin.authentic_netty.nio$
+ * NIO客户端处理器
+ * @author ll
+ * @date 2021-03-26 16:29:54
+ **/
+public class TimeClientHandler implements Runnable{
+
+    private String host;
+    private int port;
+    private Selector selector;
+    private SocketChannel socketChannel;
+    private volatile boolean stop;
+
+    public TimeClientHandler(String host, int port) {
+        this.host = host == null ? "127.0.0.1" : host;
+        this.port = port;
+        try {
+            selector = Selector.open();
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            doConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey selectionKey = null;
+                while (iterator.hasNext()) {
+                    selectionKey = iterator.next();
+                    iterator.remove();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey selectionKey) throws IOException {
+        if (selectionKey.isValid()) {
+            //判断是否链接成功
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            if (selectionKey.isConnectable()) {
+                if (socketChannel.finishConnect()) {
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    doWrite(socketChannel);
+                } else {
+                    //连接失败进程退出
+                    System.exit(1);
+                }
+            }
+            if (selectionKey.isReadable()) {
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("Now is : " + body);
+                    this.stop = true;
+                } else if (readBytes < 0) {
+                    //对端链路关闭
+                    selectionKey.cancel();
+                    socketChannel.close();
+                } else {
+                    //0字节
+                }
+            }
+        }
+    }
+
+    private void doConnect() throws IOException {
+        //如果直接链接成功，则注册到多路复用器上，发送请求消息，读应答
+        if (socketChannel.connect(new InetSocketAddress(host, port))) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        } else {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+        writeBuffer.put(req);
+        writeBuffer.flip();
+        socketChannel.write(writeBuffer);
+        if (!writeBuffer.hasRemaining()) {
+            System.out.println("Send order 2 server succeed");
+        }
+    }
+}
